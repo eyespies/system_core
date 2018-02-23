a4e32b90b48a4cec7fe61c81eae7ec49af3937d6
Justin Spies <justin@thespies.org>
2018-02-23 13:12:07 -0500

    Cleanup and reduce / minimize the integration tests

--- test/integration/ruby/inspec/controls/linux-baseline.rb
+++ test/integration/ruby/inspec/controls/linux-baseline.rb
@@ -1,18 +1,7 @@
 # rubocop:disable Naming/FileName
 
-# While this could be done via .kitchen.yml, as of 2018-01-26 the linux-baseline controls do not use attributes
-# to enable/disable IPv6 and IPv6 forwarding. Because we want those turned on and don't want the configuration reported
-# as an error, it is necessary to skip those controls below as the ability to skip controls is not possible with
-# Test Kitchen.
-include_controls 'linux-baseline' do
-  # This host requires IPv4 forwarding
-  skip_control 'sysctl-01'
-  # This host requires IPv6
-  skip_control 'sysctl-18'
-  # This host requires IPv6 forwarding
-  skip_control 'sysctl-19'
+include_controls 'system-baseline' do
+  skip_control 'papertrail' # not included in base tests
 end
-
-include_controls 'system-baseline'
 
 # rubocop:enable Naming/FileName
