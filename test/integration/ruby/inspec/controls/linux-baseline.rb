af69cf063821d33ab9de47a320608f3351cf7bf9
Justin Spies <justin@thespies.org>
2018-02-23 13:01:01 -0500

    Align with latest Rubocop version

--- test/integration/ruby/inspec/controls/linux-baseline.rb
+++ test/integration/ruby/inspec/controls/linux-baseline.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Style/FileName
+# rubocop:disable Naming/FileName
 
 # While this could be done via .kitchen.yml, as of 2018-01-26 the linux-baseline controls do not use attributes
 # to enable/disable IPv6 and IPv6 forwarding. Because we want those turned on and don't want the configuration reported
@@ -14,3 +14,5 @@ include_controls 'linux-baseline' do
 end
 
 include_controls 'system-baseline'
+
+# rubocop:enable Naming/FileName
