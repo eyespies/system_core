51459464dae8609339cd2803a21d34bd60714f45
Justin Spies <justin@thespies.org>
2018-06-14 14:28:16 -0400

    Fix incorrect configuration file name for remote_syslog2

--- attributes/logging.rb
+++ attributes/logging.rb
@@ -29,10 +29,10 @@ node.default['system_core']['papertrail']['remote_syslog2']['source_url_prefix']
 node.default['system_core']['papertrail']['remote_syslog2']['version'] = '0.20'
 node.default['system_core']['papertrail']['remote_syslog2']['service_name'] = 'remote_syslog'
 
-node.default['system_core']['papertrail']['remote_syslog2']['config']['file_name'] = '/etc/log_files.yaml'
-node.default['system_core']['papertrail']['remote_syslog2']['config']['template_file'] = '/etc/log_files.yaml'
+node.default['system_core']['papertrail']['remote_syslog2']['config']['file_name'] = '/etc/log_files.yml'
+node.default['system_core']['papertrail']['remote_syslog2']['config']['template_file'] = '/etc/log_files.yml'
 node.default['system_core']['papertrail']['remote_syslog2']['config']['template_cookbook'] = 'system_core'
-node.default['system_core']['papertrail']['remote_syslog2']['config']['template_file'] = 'log_files.yaml.erb'
+node.default['system_core']['papertrail']['remote_syslog2']['config']['template_file'] = 'log_files.yml.erb'
 
 node.default['system_core']['papertrail']['remote_syslog2']['protocol'] = 'tls'
 node.default['system_core']['papertrail']['remote_syslog2']['hostname'] = nil
