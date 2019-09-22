21f32f7ddb8358b42708cb33d5ccc98dc637e986
Justin Spies <justin@thespies.org>
2019-09-22 08:24:42 -0400

    (fix) update to Fauxhai supported OS versions

--- spec/platforms.rb
+++ spec/platforms.rb
@@ -6,10 +6,10 @@ def platforms
     },
     'centos' => {
       # CentOS versions don't 100% match those from Oracle Linux
-      'versions' => ['6.8', '7.5.1804']
+      'versions' => ['6.10', '7.6.1810']
     },
     'oracle' => {
-      'versions' => ['6.8', '7.2']
+      'versions' => ['6.10', '7.6']
     }
   }
 end
