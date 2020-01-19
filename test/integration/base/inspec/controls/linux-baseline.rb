85c83da1668c448072bf403ed828f58b62034291
Justin Spies <justin@thespies.org>
2020-01-19 14:25:07 -0500

    (fix) do not test hostname in the base tests - Vagrant / VirtualBox defaults to no domain name

--- test/integration/base/inspec/controls/linux-baseline.rb
+++ test/integration/base/inspec/controls/linux-baseline.rb
@@ -2,6 +2,7 @@
 
 include_controls 'system-baseline' do
   skip_control 'papertrail' # not included in base tests
+  skip_control 'hostname' # no hostname is set in the base tests
 end
 
 # rubocop:enable Naming/FileName
