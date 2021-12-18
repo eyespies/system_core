9b38028791ebfdf4fe6023974b26652ae801b447
Justin Spies <justin@thespies.org>
2021-12-18 17:41:06 -0500

    (fix) remove references to hostname cookbook that is superceded by the hostname resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,32 +16,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# If the node name already has the domain component included, then use the node name itself; otherwise
-# use the domain.
-
-# NOTE: This appends the domain name if the specified domain name is not part of the hostname, even if the hostname
-# already includes a domain name. e.g. if node['system_core']['domain'] is 'example.com' and the node_name is
-# 'host.mydomain.org', set_fqdn will be set to 'host.mydomain.org.example.com'
-# If the node_name contains a period do not append the node['system_core']['domain'] value.
-node.override['set_fqdn'] = if Chef::Config[:node_name] =~ /#{node['system_core']['domain']}$/i ||
-                               Chef::Config[:node_name] =~ /.*\..*/
-                              Chef::Config[:node_name]
-                            else
-                              "*.#{node['system_core']['domain']}"
-                            end
-
-# The default is 127.0.1.1, which is used under Debian, not RHEL based systems.
-node.override['hostname_cookbook']['hostsfile_ip'] = '127.0.0.1' if platform_family?('rhel')
-
-# Without this line, the 'hostname' cookbook sets:
-#  -127.0.0.1 full-oracle-73.linux.example.com full-oracle-73
-#  +127.0.0.1 full-oracle-73.linux.example.com full-oracle-73 localhost
-#
-# but then a moment letter sets:
-#  -127.0.0.1 full-oracle-73.linux.example.com full-oracle-73 localhost
-#  +127.0.0.1 full-oracle-73.linux.example.com full-oracle-73
-#
-# If using a real IP for the hostfile_ip attribute, then this should not be used.
-node.override['hostname_cookbook']['hostsfile_aliases'] = ['localhost']
-include_recipe 'hostname'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe 'system_core::default' do
-  let(:recipes) do
-    %w(hostname)
-  end
-
   platforms.each do |platform, details|
     versions = details['versions']
     versions.each do |version, opts|
@@ -19,13 +15,6 @@ describe 'system_core::default' do
           runner = ChefSpec::SoloRunner.new(platform: platform, version: version, path: opts['fixture_path'])
           runner.node.override['environment'] = 'dev'
           runner.converge(described_recipe)
-        end
-
-        it 'should include the other recipes' do
-          recipes.each do |recipe|
-            expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with(recipe)
-          end
-          chef_run
         end
       end
     end
